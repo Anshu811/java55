e554e8a036619865fc940a048c47d1b2491a02b8
Anshu <anshukumari889900@gmail.com>
2025-08-27 09:34:04 +0530

    Exception

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+   // public static void main(String[] args) {
+       // schoolstudent ss=new schoolstudent(12,"anshu");
+       // ss.attend_class();
+        //ss.give_exam();
+        //ss.register();
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    //}
+//}
