9a32ff67432fcfcb300184dbd51aa78e036be3c6
Anshu <anshukumari889900@gmail.com>
2025-09-03 09:42:10 +0530

    collection 03/09/2025

--- src/myexception.java
+++ src/myexception.java
@@ -1,14 +1,14 @@
-public class myexception {
-    public static void main(String[] args) {
-        userdefinedexception udf=new userdefinedexception();
-        try{
-            udf.validate(19);
-            udf.validate(12);
-
-        }
-        catch(Invalidageexception e){
-            System.out.println(e.getMessage());
-        }
-
-    }
-}
+//public class myexception {
+//    public static void main(String[] args) {
+//        userdefinedexception udf=new userdefinedexception();
+//        try{
+//            udf.validate(19);
+//            udf.validate(12);
+//
+//        }
+//        catch(Invalidageexception e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+//}
